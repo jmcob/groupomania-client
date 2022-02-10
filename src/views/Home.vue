37f6ce3b83f6d583347a6f80c84070d75089edba
jmcob <jmcob@pm.me>
2022-02-10 23:49:31 +0100

    -Footer Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
         <HelloWorld />
-        <Footer />
 </template>
 
 <script>
-import Footer from "../components/Footer";
 import HelloWorld from "../components/HelloWorld";
 
 export default {
         name: "Home",
         components: {
                 HelloWorld,
-                Footer,
         },
 };
 </script>
